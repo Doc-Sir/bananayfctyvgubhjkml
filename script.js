a34f1e58d63f80a777cd7e53c1da8e6cddee9e01
Doc-Sir <145786854+Doc-Sir@users.noreply.github.com>
2023-12-04 11:23:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,37 +2,29 @@ window.onload = function() {
   var notification = document.getElementById('notification');
   var earthDayButton = document.getElementById('vallis-cold');
   var earthNightButton = document.getElementById('vallis-warm');
+  
+  function checkImage(imageName, expectedImage) {
+  var img = new Image();
+  img.src = imageName;
 
-  earthDayButton.addEventListener('click', function() {
-    checkImage('thing2.png');
-  });
-
-  earthNightButton.addEventListener('click', function() {
-    checkImage('thing.png');
-  });
-
-  function checkImage(imageName) {
-    var img = new Image();
-    img.src = imageName;
-
-    img.onload = function() {
+  img.onload = function() {
+    if (img.src.includes(expectedImage)) {
       showNotification('Image found!');
-    };
-
-    img.onerror = function() {
+    } else {
       showNotification('Image not found!');
-    };
-  }
-
-  function showNotification(message) {
-    if (Notification.permission === 'granted') {
-      new Notification('Image Checker', { body: message });
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then(function(permission) {
-        if (permission === 'granted') {
-          new Notification('Image Checker', { body: message });
-        }
-      });
     }
-  }
+  };
+
+  img.onerror = function() {
+    showNotification('Image not found!');
+  };
+}
+
+earthDayButton.addEventListener('click', function() {
+  checkImage('thing.png', 'day');
+});
+
+earthNightButton.addEventListener('click', function() {
+  checkImage('thing2.png', 'night');
+});
 };
